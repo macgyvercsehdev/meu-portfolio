4ddd867852f820ceaecdff8383460646095767a3
Macgyver Cseh dos Santos <86620052+macgyvercsehdev@users.noreply.github.com>
2025-06-19 23:55:26 +0000

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exemplo: Rolagem suave para as seções quando clicar nos links da navegação
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
